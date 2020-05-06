1f4e6f6ec616637e48609180a44d3a26d8631acd
vkxmfkdl <vkxmfkdl@gmail.com>
2020-05-06 12:33:03 +0900

    2020-05-06 박경민 record페이지 수정

--- src/main/java/com/jojoldu/book/springboot/web/Dto/PostsListResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/Dto/PostsListResponseDto.java
@@ -3,19 +3,22 @@ package com.jojoldu.book.springboot.web.Dto;
 import com.jojoldu.book.springboot.domain.posts.Posts;
 import lombok.Getter;
 
+import javax.swing.text.DateFormatter;
+import java.text.DateFormat;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Getter
 public class PostsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
-
+    private String modifiedDate;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     public PostsListResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
+        this.modifiedDate = entity.getModifiedDate().format(this.formatter);
     }
 }
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -28,7 +28,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 등록되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/record';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -49,7 +49,7 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
-            window.location.href = '/';
+            window.location.href = '/record';
         }).fail(function (error) {
             alert(JSON.stringify(error));
         });
@@ -62,7 +62,7 @@ var main = {
             url:'/api/v1/posts/'+id
         }).done(function(){
             alert('글이 삭제되었습니다.');
-            window.location.href ='/';
+            window.location.href ='/record';
         }).fail(function(error){
             alert(JSON.stringify(error));
         })
